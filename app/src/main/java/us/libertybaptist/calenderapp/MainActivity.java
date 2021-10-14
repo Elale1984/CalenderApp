f53d3327d4a4805e658cec8b2f92b00f892cda39
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-10-14 00:31:38 -0500

    Added style for dialog box to control title. altered padding for UI look and feel of selected day dialog

--- app/src/main/java/us/libertybaptist/calenderapp/MainActivity.java
+++ app/src/main/java/us/libertybaptist/calenderapp/MainActivity.java
@@ -19,14 +19,20 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
 
+    // textview for the month and year at top of calender
     private TextView monthYearText;
+
+    // recycler view for the days in each month
     private RecyclerView calenderRecyclerView;
+
+    //local date will be the selected date when clicked.
     private LocalDate selectedDate;
 
     // the following is for onclick of date
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-    private TextView tv_AddEvent, tv_ClearAllEvents;
+    private TextView tvSelectedDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +43,21 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         setMonthView();
     }
 
+    //initialize the calender recycler view and textview for month/year at top of calender
     private void initWidgets() {
         calenderRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYearText = findViewById(R.id.monthYearTV);
 
     }
 
+    //sets the view for the month, creates array for days in currently selected month and calls
+    //adapter. Invokes the layout manager and sets to gridlayout for the calender. sets the layout
+    // manager and the adapter
     private void setMonthView() {
+        //sets the text of the month/year at top of calender
         monthYearText.setText(monthYearFromDate(selectedDate));
+
+        //method call to get the days in current month tp fill array
         ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
 
 
@@ -54,6 +67,7 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         calenderRecyclerView.setAdapter(calenderViewAdapter);
     }
 
+    //helper method to fill daysInMonth array. takes in the local date.
     private ArrayList<String> daysInMonthArray(LocalDate date) {
         ArrayList<String> daysInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(date);
@@ -62,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
         int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
+        //setting the text in each grid location for the days of the month by filling the array
         for(int i = 1; i <= 42; i++ ){
             if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
                 daysInMonthArray.add("");
@@ -74,54 +89,71 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         return daysInMonthArray;
     }
 
+    //formatter for the date
     private String monthYearFromDate(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return date.format(formatter);
     }
 
+    //button click event for the previous month arrow
     public void PreviousMonthAction(View view) {
+        //sets selectedDate to previous month
         selectedDate = selectedDate.minusMonths(1);
         setMonthView();
 
     }
 
+    //button click event for the next month arrow
     public void NextMonthAction(View view) {
+        //sets selectedDate to next month
         selectedDate = selectedDate.plusMonths(1);
         setMonthView();
     }
 
+    //onItemClick handles the click event for each day of the month
     @Override
     public void onItemClick(int position, String dayText) {
+        //toast for empty squares
         if(dayText.equals("")){
             String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         }
+        //calls onClickEvents for each real day of the month
         else{
-            onClickEvents();
+
+            onClickEvents(dayText);
         }
     }
 
-    public void onClickEvents() {
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View optionsView = getLayoutInflater().inflate(R.layout.date_event_controls, null);
-        tv_AddEvent = findViewById(R.id.tv_AddEvent);
-        tv_ClearAllEvents = findViewById(R.id.tv_RemoveAllEvents);
+    //launches the layout for options on each day for event adding and viewing
+    public void onClickEvents(String dayText) {
 
+
+        String message =  dayText + " " + monthYearFromDate(selectedDate);
+
+
+        dialogBuilder = new AlertDialog.Builder(this, R.style.CustomDialogTheme);
+        final View optionsView = getLayoutInflater().inflate(R.layout.date_event_controls, null);
+
+        dialogBuilder.setTitle(message);
         dialogBuilder.setView(optionsView);
         dialog = dialogBuilder.create();
         dialog.show();
 
+
     }
 
-
+    // takes user to layout for adding events for that day
     public void AddEventClickListener(View view) {
         Toast.makeText(MainActivity.this, "add Event", Toast.LENGTH_SHORT).show();
+
         dialog.cancel();
 
     }
 
+    // takes user to layout for viewing all events for that day
     public void RemoveAllEventsClickListener(View view) {
-        Toast.makeText(MainActivity.this, "Delete All Event", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "View All Events", Toast.LENGTH_SHORT).show();
         dialog.cancel();
     }
 }
